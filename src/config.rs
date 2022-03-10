24f02e004f4f52f5d78d01dc3bbfe405a5e2c6e1
Jens Reimann <jreimann@redhat.com>
2022-03-10 14:31:03 +0100

    chore: update dependencies

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,11 @@ use serde::Deserialize;
 
 pub trait ConfigFromEnv<'de>: Sized + Deserialize<'de> {
     fn from_env() -> Result<Self, config::ConfigError> {
-        Self::from(config::Environment::new())
+        Self::from(config::Environment::default())
     }
 
     fn from_env_prefix<S: AsRef<str>>(prefix: S) -> Result<Self, config::ConfigError> {
-        Self::from(config::Environment::with_prefix(&format!(
-            "{}_",
-            prefix.as_ref()
-        )))
+        Self::from(config::Environment::with_prefix(prefix.as_ref()))
     }
 
     fn from(env: config::Environment) -> Result<Self, config::ConfigError>;
@@ -17,8 +14,9 @@ pub trait ConfigFromEnv<'de>: Sized + Deserialize<'de> {
 
 impl<'de, T: Deserialize<'de> + Sized> ConfigFromEnv<'de> for T {
     fn from(env: config::Environment) -> Result<T, config::ConfigError> {
-        let mut cfg = config::Config::new();
-        cfg.merge(env.separator("__"))?;
-        cfg.try_into()
+        let cfg = config::Config::builder()
+            .add_source(env.separator("__"))
+            .build()?;
+        cfg.try_deserialize()
     }
 }
